c0ab825134593932055814e490f732f3dd2b7647
enesdemirelus <enesdemirelus@gmail.com>
2025-06-12 00:18:16 -0500

    added logo

--- enes-ml-lab/app/components/ThemeToggle.tsx
+++ enes-ml-lab/app/components/ThemeToggle.tsx
@@ -1,9 +1,12 @@
 "use client";
 import { useEffect, useState } from "react";
-
 import React from "react";
 
-const ThemeToggle = () => {
+interface ThemeToggleProps {
+  fontClass: string;
+}
+
+const ThemeToggle = ({ fontClass }: ThemeToggleProps) => {
   const [darkMode, setDarkMode] = useState(false);
 
   useEffect(() => {
@@ -14,13 +17,13 @@ const ThemeToggle = () => {
     <>
       <button
         type="button"
-        className="text-gray-900 bg-white dark:bg-gray-800 dark:text-white 
-             border border-gray-300 dark:border-gray-600 
-             rounded-lg text-sm px-5 py-2.5 me-2 mb-2 font-medium 
-             transition-all duration-200 ease-in-out 
-             transform hover:scale-105 active:scale-95 
-             hover:bg-gray-100 dark:hover:bg-gray-700 
-             focus:outline-none"
+        className={`${fontClass} text-gray-900 bg-white dark:bg-gray-800 dark:text-white 
+        border border-gray-300 dark:border-gray-600 
+        rounded-lg text-sm px-5 py-2.5 me-2 mb-2 font-medium 
+        transition-all duration-200 ease-in-out 
+        transform hover:scale-105 active:scale-95 
+        hover:bg-gray-100 dark:hover:bg-gray-700 
+        focus:outline-none`}
         onClick={() => setDarkMode(!darkMode)}
       >
         {darkMode ? "☀️ Light Mode ☀️" : "🌕 Dark Mode 🌕"}
--- enes-ml-lab/app/page.tsx
+++ enes-ml-lab/app/page.tsx
@@ -1,23 +1,33 @@
 import { Oldenburg } from "next/font/google";
 import ThemeToggle from "./components/ThemeToggle";
+import Image from "next/image";
+import logo from "./favicon.ico";
 
 const lato = Oldenburg({ subsets: ["latin"], weight: ["400"] });
 
 export default function Home() {
   return (
-    <>
-      <div className="flex flex-col justify-center items-center h-screen px-4 text-center ">
-        <h1 className={`${lato.className} text-4xl`}>Hello Friend! 👋</h1>
-        <h2 className={`${lato.className} text-xl m-2`}>
-          My name is Enes Demirel 🇹🇷, and I am a Math and Computer Science
-          Student 👨‍💻.
-        </h2>
-        <h3 className={`${lato.className} text-xl`}>
-          I will showcase some of my Machine Learning Projects here.
-        </h3>
-        <h2 className={`${lato.className} text-xl m-2`}>STAY TUNED ... ⏳</h2>
-        <ThemeToggle></ThemeToggle>
+    <div className="flex flex-col justify-center items-center h-screen px-4 text-center">
+      <Image
+        src={logo}
+        alt="Enes ML Lab Logo"
+        className="w-30 h-30 mb-2"
+        priority
+      />
+
+      <h1 className={`${lato.className} text-4xl m-`}>Hello Friend! 👋</h1>
+      <h2 className={`${lato.className} text-xl m-2`}>
+        My name is Enes Demirel 🇹🇷, and I am a Math and Computer Science Student
+        👨‍💻.
+      </h2>
+      <h3 className={`${lato.className} text-xl`}>
+        I will showcase some of my Machine Learning Projects here.
+      </h3>
+      <h2 className={`${lato.className} text-xl m-2`}>STAY TUNED ... ⏳</h2>
+
+      <div className="absolute top-4 right-4">
+        <ThemeToggle fontClass={lato.className} />
       </div>
-    </>
+    </div>
   );
 }
