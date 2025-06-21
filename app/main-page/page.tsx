68b156a0e6b13cba29442093817327ada804204e
Enes <enes@airblox.com>
2025-06-20 21:58:15 -0500

    created grid view for main-menu

--- app/main-page/page.tsx
+++ app/main-page/page.tsx
@@ -1,14 +1,24 @@
 import React from "react";
+import { Card, CardContent } from "@/components/ui/card";
 import ThemeToggle from "../components/ThemeToggle";
-import { Button } from "@/components/ui/button";
 
 const MainPage = () => {
   return (
-    <div className="flex justify-center items-center h-screen text-6xl text-center">
-      WELCOME TO MAIN PAGE!
-      <Button>Hello!</Button>
-      <ThemeToggle></ThemeToggle>
-    </div>
+    <>
+      <div className="min-h-screen flex items-center justify-center m-2">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 w-full max-w-6xl px-4">
+          {Array.from({ length: 9 }).map((_, index) => (
+            <Card key={index} className="w-full h-40">
+              <CardContent className="h-full flex items-center justify-center">
+                <h1 className="text-center text-xl font-medium">
+                  Coming Soon...
+                </h1>
+              </CardContent>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
